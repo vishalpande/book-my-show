f43dbb4fa58971dba5d483c7f4c0614073be69aa
vishalpande <pandevishal40@gmail.com>
2023-08-10 15:16:56 +0530

    add validaions

--- backend/controller/bookMovieController.js
+++ backend/controller/bookMovieController.js
@@ -1,34 +1,35 @@
 const { connection } = require("../connector");
 
 class BookMovieController {
-    static async getFunction(request,responce){
-        await connection.findOne().sort({ field: 'asc', _id: -1 }).limit(1).exec((err, bookMovie)=>{
-            if(err){
-                responce.status(500).json({error: err})
-            }else {
-                if (bookMovie===null){
-                    responce.status(200).json({"message": "no previous booking found"})
-                }else{
-                    responce.status(200).json(bookMovie)
-                }
-                
-            }
-        }) 
-    }
+  static async getFunction(request, responce) {
+    await connection
+      .findOne()
+      .sort({ field: "asc", _id: -1 })
+      .limit(1)
+      .exec((err, bookMovie) => {
+        if (err) {
+          responce.status(500).json({ error: err });
+        } else {
+          if (bookMovie === null) {
+            responce.status(200).json({ message: "no previous booking found" });
+          } else {
+            responce.status(200).json(bookMovie);
+          }
+        }
+      });
+  }
 
-    static async postFuntion(request,responce) {
-        const data = request.body
-        const bookMovie = new connection(data)
-        await bookMovie.save((err)=>{
-            if (err){
-                responce.status(500).json({error: err})
-            }
-            else{
-                responce.status(200).json({message: "successful added"})
-            }
-        });
-    }
-
+  static async postFuntion(request, responce) {
+    const data = request.body;
+    const bookMovie = new connection(data);
+    await bookMovie.save((err) => {
+      if (err) {
+        responce.status(500).json({ error: err });
+      } else {
+        responce.status(200).json({ message: "successful added" });
+      }
+    });
+  }
 }
 
-module.exports = BookMovieController
+module.exports = BookMovieController;
--- backend/index.js
+++ backend/index.js
@@ -1,23 +1,19 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const bookMovieRouter = require('./router/bookMovieRouter')
-const cors = require('cors')
+const bookMovieRouter = require("./router/bookMovieRouter");
+const cors = require("cors");
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.use(cors())
-app.use('/api/booking',bookMovieRouter)
+app.use(cors());
+app.use("/api/booking", bookMovieRouter);
 // if other then  /api/booking will
 app.use((req, res, next) => {
-    return res.json({ message: 'page not found!'});
+  return res.json({ message: "page not found!" });
 });
-
-
-
-
 
 const port = 8080;
 app.listen(port, () => console.log(`App listening on port ${port}!`));
 
-module.exports = app;   
+module.exports = app;
